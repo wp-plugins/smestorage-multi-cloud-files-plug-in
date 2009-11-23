fc2266c931351d9ea8c01936e629d9e848a324e5
smestorage <smestorage@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-11-23 18:07:12 +0000

    fixed backup location
    git-svn-id: https://plugins.svn.wordpress.org/smestorage-multi-cloud-files-plug-in/trunk@176244 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ sme/lib.php
@@ -0,0 +1,92 @@
+<?
+
+define('_SERVICE_URL','http://www.smetube.com/smestorage/api/');
+
+require("http.php");
+require('class.xmltoarray.php');
+
+
+/**
+* Takes REST command, process request and return array created from returned xml document
+* 
+**/
+function processRequest($request,$debug=0,$data=array(),$files=array()){
+	$result=array('',array());
+	$http=new http_class;
+	//$http->timeout=0;
+	//$http->data_timeout=0;
+	$http->debug=($debug==2)?1:0;
+	$http->html_debug=1;
+
+	$url=_SERVICE_URL.$request;
+//	echo $url.'<br>';
+	if($debug>0){
+		echo $url.'<br>';
+		if($debug>1)
+			exit;
+	}
+	//exit;
+	//return;
+	$error=$http->GetRequestArguments($url,$arguments);
+	$arguments["RequestMethod"]="POST";
+	$arguments["PostValues"]=array(
+		
+	);
+	foreach (array_keys($data) as $d)
+		$arguments["PostValues"][$d]=$data[$d];
+
+	if(count($files)>0 ){
+		$arguments["PostFiles"]=array();
+		$arguments["PostValues"]["MAX_FILE_SIZE"]="10000000";
+		foreach(array_keys($files) as $f)
+			$arguments["PostFiles"][$f]=array('FileName'=>$files[$f],"Content-Type"=>"automatic/name");
+	}
+		//print_r($arguments["PostFiles"]);
+	//$arguments["Referer"]="http://www.alltheweb.com/";
+	$result[0]=$http->Open($arguments);
+
+	if($result[0]=="")
+	{
+		$result[0]=$http->SendRequest($arguments);
+
+		if($result[0]=="")
+		{
+			$result[0]=$http->ReadReplyHeaders($headers);
+
+			if($result[0]=="")
+			{
+
+				$result[0]=$http->ReadReplyBody($body,1000000);
+				if($debug>0)
+					echo $body;
+				$x=new XmlToArray($body);
+				$a=$x->createArray();
+				$a=$a['response'];
+				if($a['status']!='ok')
+					$result[0]=$a['statusmessage'];
+				if($debug>0){
+					print_r($a);
+					exit;
+				}
+				$result[1]=$a;
+				
+			}
+		}
+		$http->Close();
+	}
+	return $result;
+		
+		
+}
+/**
+* Encode arguments with base64 encoding and join with comma delimiter
+**/
+function encodeArgs($args){
+	$a=array();
+	foreach ($args as $ar){
+		$a[]=base64_encode($ar);
+	}
+	return join(',',$a);
+}
+
+?>
